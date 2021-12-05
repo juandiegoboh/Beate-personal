3ad93901e2a8d09f524d716f440002df96df05a2
juandiegoboh <juandiegoboh@hotmail.com>
2021-12-05 16:39:04 -0500

    Implementaacion frontend en backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,21 +7,33 @@ const expressJwt = require('express-jwt');
 const helmet = require('helmet');
 const PORT = process.env.PORT || 3000;
 
-
 const app = express();
+
+// Carpeta public para acceder al frontend
 app.use(express.json());
-app.use(cors());
+app.use(express.static('public'));
 app.use(helmet());
 
-// app.use(
-//     expressJwt({
-//         secret: process.env.JSON_SECRET,
-//         algorithms: ['HS256'],
-//         requestProperty: process.env.requestProperty
-//     }).unless({
-//         path: ['/api/login', '/api/register']
-//     })
-// );
+var whitelist = ['http://example1.com', 'http://example2.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { Origin: true } // reflect (enable) the requested Origin in the CORS response
+  }else{
+    corsOptions = { Origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(
+    expressJwt({
+        secret: process.env.JSON_SECRET,
+        algorithms: ['HS256'],
+        requestProperty: process.env.requestProperty
+    }).unless({
+        path: ['/api/login', '/api/register']
+    })
+);
 
 app.use('/api/register', require('./users/register'))
 app.use('/api/login', require('./users/login'));
